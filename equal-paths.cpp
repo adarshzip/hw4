663e29e24903304b144aa7c07d330843f798b542
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-11-11 20:21:25 -0800

    part 1 no debug yet

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,45 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int pathLength(Node* node){
+    if (node == nullptr){ // empty node
+        return 0; 
+    }
+    else {
+        int left = pathLength(node->left); 
+        int right = pathLength(node->right);
+        if (left == -1 || right == -1){
+            return -1; 
+        }
+        else if (left == 0 && right == 0){
+            return 1; 
+        }
+        else if (left != 0 && right != 0 && left != right){
+            return -1; 
+        }
+        else {
+            if(left > right){
+                return 1 + left;
+            }
+            else {
+                return 1 + right; 
+            }
+        }
+    }
+}
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    if (root == nullptr){
+        return true; 
+    }
+    
+    if (pathLength(root) == -1){
+        return false; 
+    }
+    else {
+        return true; 
+    }
 }
 
